9f85d41eda9c2533f32aa5ccffea95c09f81efcf
Michael <andaloromichael@gmail.com>
2020-02-13 14:30:14 -0600

    Updated to reflect Lambda notation

--- hw3-part1/src/main.rs
+++ hw3-part1/src/main.rs
@@ -2,35 +2,40 @@ use std::io;
 
 // Main function that calls the calculating functions
 fn main() {
-    let plus_one = |x: i32| x + 1;
     // Get user input for a
-    let mut data_01 = String::new(); // make a mutable string variable
+    let mut a = String::new(); // make a mutable string variable
     println!("Enter a.");
-    io::stdin().read_line(&mut data_01) // Get a from the user
+    io::stdin().read_line(&mut a) // Get a from the user
                .ok()
                .expect("Incorrect input"); // Error handling
-    let data_01: i32 = data_01.trim().parse().unwrap(); // Convert string into int
+    let a: i32 = a.trim().parse().unwrap(); // Convert string into int
 
     // Get user input for b
-    let mut data_02 = String::new(); // make a mutable string variable
+    let mut b = String::new(); // make a mutable string variable
     println!("Enter b.");
-    io::stdin().read_line(&mut data_02) // Get b from the user
+    io::stdin().read_line(&mut b) // Get b from the user
                .ok()
                .expect("Incorrect input"); // Error handling
-    let data_02: i32 = data_02.trim().parse().unwrap(); // Convert string into int
+    let b: i32 = b.trim().parse().unwrap(); // Convert string into int
 
     // Call the function
-    sum_of_squares(data_01, data_02);
+    sum_of_squares(a, b);
 }
 
-// Function to square a number
+/*
+// Function to square a number ... Changed into lambda notation within the sum_of_squares function
 fn square(x: i32) -> i32{
     let x = x * x;
     return x;
 }
+*/
 
 // Main function with all the code in it. Takes two inputs: a, b
 fn sum_of_squares(a: i32, b: i32) {
+
+    // ****************Square a number (LAMBDA NOTATION)*****************
+    let squared = |x: i32| x * x;
+
     // If a is greater than b, the function ends
     if a > b{
         println!("There is no sum of squares available for your selection of {} and {}.", a, b);
@@ -41,7 +46,7 @@ fn sum_of_squares(a: i32, b: i32) {
         // Sets the beginning number. We are calculating between a and b.
         let mut counter = a+1;
         while counter < b{
-            sum_of_squares_answer = sum_of_squares_answer + square(counter);
+            sum_of_squares_answer = sum_of_squares_answer + squared(counter);
             counter = counter + 1;
         }
         println!("The sum of squares between {} and {} is {}.", a, b, sum_of_squares_answer);
