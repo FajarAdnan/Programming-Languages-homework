bbe6d770e20daf22af905aa4fabc79a865427162
Michael <andaloromichael@gmail.com>
2020-02-18 08:19:14 -0600

    Correct homework names

---
+++ hw4/target/debug/deps/hw4-dad114c809cbc201.d
@@ -0,0 +1,5 @@
+/Users/michaelandaloro/Documents/GitHub/Programming-Languages-homework/hw4/target/debug/deps/hw4-dad114c809cbc201: src/main.rs
+
+/Users/michaelandaloro/Documents/GitHub/Programming-Languages-homework/hw4/target/debug/deps/hw4-dad114c809cbc201.d: src/main.rs
+
+src/main.rs:
---
+++ hw4/target/debug/hw4.d
@@ -0,0 +1,1 @@
+/Users/michaelandaloro/Documents/GitHub/Programming-Languages-homework/hw4/target/debug/hw4: /Users/michaelandaloro/Documents/GitHub/Programming-Languages-homework/hw4/src/main.rs
